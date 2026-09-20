eccb75b67da43e75ab945798d0e8c1949ba74ca1
sargesticky-code <sargesticky@gmail.com>
2026-09-20 08:01:39 +0800

    Export Multibetter bridge models

--- multibetter/src/multibetter/__init__.py
+++ multibetter/src/multibetter/__init__.py
@@ -1,5 +1,17 @@
 """Multibetter V1."""
 
-from .models import CanonicalFixture, SourcePrediction, Market, MatchDecision
+from .models import (
+    CanonicalFixture,
+    MatchDecision,
+    Market,
+    MultiSourceFixture,
+    SourcePrediction,
+)
 
-__all__ = ["CanonicalFixture", "SourcePrediction", "Market", "MatchDecision"]
+__all__ = [
+    "CanonicalFixture",
+    "MatchDecision",
+    "Market",
+    "MultiSourceFixture",
+    "SourcePrediction",
+]
