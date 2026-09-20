bdf4287fe3a03c20493f923cd800b6735485ebb4
sargesticky-code <sargesticky@gmail.com>
2026-09-20 08:33:05 +0800

    Add Multibetter current intake package

---
+++ multibetter/src/multibetter/intake/__init__.py
@@ -0,0 +1,15 @@
+from .accumulator import collect_accumulator
+from .betclan import collect_betclan
+from .footballsupertips import collect_footballsupertips
+from .forebet_feed import collect_forebet_from_our_feed
+from .primatips import collect_primatips
+from .statarea import collect_statarea
+
+__all__ = [
+    "collect_accumulator",
+    "collect_betclan",
+    "collect_footballsupertips",
+    "collect_forebet_from_our_feed",
+    "collect_primatips",
+    "collect_statarea",
+]
