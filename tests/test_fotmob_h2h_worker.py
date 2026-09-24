658edb302e091f096eb2549ab337ef1fb79cbed6
sargesticky-code <sargesticky@gmail.com>
2026-09-24 23:55:24 +0800

    Test FotMob H2H last-good reuse

--- tests/test_fotmob_h2h_worker.py
+++ tests/test_fotmob_h2h_worker.py
@@ -26,6 +26,25 @@ class FotMobWorkerTests(unittest.TestCase):
             def matches(self,date): raise RuntimeError("down")
         out=build([self.row()],[],Broken(),10)
         self.assertEqual(out[0]["quality"],"SOURCE_UNAVAILABLE")
+    def test_previous_h2h_is_reused_without_detail_call(self):
+        class Counting(Fake):
+            def __init__(self): self.details=0
+            def match_details(self,match_id):
+                self.details+=1
+                return super().match_details(match_id)
+        client=Counting()
+        previous=[{
+            **self.row(),
+            "fotmob_match_id":"321","mapping_status":"VERIFIED","mapping_reason":"exact_pair_and_kickoff",
+            "quality":"H2H_OK","h2h_games":"2","home_wins":"1","draws":"1","away_wins":"0",
+            "home_goals":"3","away_goals":"1","avg_total_goals":"2.00","last5":"HD",
+            "meetings_json":'[{"result":"H"},{"result":"D"}]'
+        }]
+        out=build([self.row()],[],client,10,previous_rows=previous)
+        self.assertEqual(out[0]["quality"],"H2H_OK")
+        self.assertEqual(out[0]["last5"],"HD")
+        self.assertEqual(client.details,0)
+
     def test_budget_is_deferred(self):
         out=build([self.row()],[],Fake(),0)
         self.assertEqual(out[0]["quality"],"DEFERRED_BUDGET")
